f8c976234e59959c2f0b8a0428b3a7019a609a74
zfleury <zach@fleury.lol>
2023-12-29 16:39:58 -0600

    skill issue

--- index.js
+++ index.js
@@ -24,10 +24,8 @@ function closeModal(modalId) {
 
 cutleryKit = false;
 cups = false;
-let mintWipe = 0;
 
 function dryPack() {
-  mintWipe = 0;
   document.getElementById("kits").style.display = "none";
   document.getElementById("napkins").style.display = "none";
   document.getElementById("cups").style.display = "none";
@@ -35,23 +33,20 @@ function dryPack() {
   let userInput = document.getElementById("guestCount").value;
   let guestCount = parseInt(userInput, 10);
   document.getElementById("packCount").innerHTML = guestCount;
-  console.log(guestCount);
   let mintWipe = Math.floor(guestCount / 25) + (guestCount % 25 > 0 ? 1 : 0);
   document.getElementById("mintWipe").innerHTML = Math.floor(mintWipe);
-}
 
-if (cutleryKit == true) {
-  let cutleryCount = guestCount;
-  document.getElementById("kits").style.display = "inline-flex";
-  console.log(`${cutleryCount} Cutlery Kits`);
-} else {
-  document.getElementById("napkins").style.display = "inline-flex";
-}
+  if (cutleryKit == true) {
+    let cutleryCount = guestCount;
+    document.getElementById("kits").style.display = "inline-flex";
+  } else {
+    document.getElementById("napkins").style.display = "inline-flex";
+  }
 
-if (cups == true) {
-  let cupCount = guestCount;
-  document.getElementById("cups").style.display = "inline-flex";
-  console.log(`${cupCount} Cups`);
+  if (cups == true) {
+    let cupCount = guestCount;
+    document.getElementById("cups").style.display = "inline-flex";
+  }
 }
 
 function trayYes() {
